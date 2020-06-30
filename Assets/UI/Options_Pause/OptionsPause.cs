66f550559d7a245727462fa11f583cb2dcf7526d
celesGye <gdwangwangwang@gmail.com>
2020-06-30 09:24:19 +0800

    eee

--- Assets/UI/Options_Pause/OptionsPause.cs
+++ Assets/UI/Options_Pause/OptionsPause.cs
@@ -6,7 +6,6 @@ using UnityEngine.SceneManagement;
 public class OptionsPause : MonoBehaviour
 {
     public GameObject pauseMenu;
-    
 
     public void OnEnable()
     {
@@ -26,6 +25,11 @@ public class OptionsPause : MonoBehaviour
     public void Quit()
     {
         GameObject.FindObjectOfType<GameManager>().GetComponent<GameManager>().QuitGame();
+    }
+
+    public void Settings()
+    {
+        // setting menu
     }
 
     public void Exit()
